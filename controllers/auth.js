1ed58a010cd15a2577d83123dd76a91e93825d0c
Nahid Ahmadi <nahid.ahmadi@codetoinspire.org>
2022-01-08 05:02:16 +0430

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
+  // console.log(req.session.isLoggedIn)
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5d3d49cab3914004072854bc')
+  .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+      
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
